566d59fa222331b6af484c8422db6e2abe9ada98
jesus <chuyelregio5@yahoo.com>
2017-09-12 20:57:22 -0700

    continuing with CRUD app

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,64 @@
 |
 */
 
+use App\Staff;
+use App\Photo;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/create', function () {
+
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path' => 'example.jpg']);
+
+});
+
+Route::get('/read', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    foreach ($staff->photos as $photo) {
+        return $photo->path;
+    }
+
+});
+
+Route::get('/update', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "updated example.jpg";
+
+    $photo->save();
+
+});
+
+Route::get('/delete', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
